5ae83d45151dedc8c2e96d07d74bef9206a2a3bd
Timur Gilemzyanov <virtualxself@gmail.com>
2020-05-13 20:49:31 +0200

    Вторая отправка на ревью

--- script.js
+++ script.js
@@ -4,16 +4,10 @@ const popup = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button'); // кнопка редактирования профиля
 const closeButton = document.querySelector('.popup__close-button'); // кнопка закрытия попапа
 const submitButton = document.querySelector('.popup__submit-button'); // кнопка сохранения изменений
-let nameInput = document.querySelector('.popup__name-input'); // поле ввода имени
-let jobInput = document.querySelector('.popup__job-input'); // поле ввода подписи
+let nameInput = document.querySelector('#name-input'); // поле ввода имени
+let jobInput = document.querySelector('#job-input'); // поле ввода подписи
 let name = document.querySelector('.profile__name'); // отражение имени в профиле
 let job = document.querySelector('.profile__job'); // отражение подписи в профиле
-
-
-// Делаем так, чтобы поля формы 
-// были заполнены при открытии попапа
-nameInput.value = name.textContent;
-jobInput.value = job.textContent;
 
 
 // Настраиваем кнопку редактирования профиля
@@ -21,6 +15,8 @@ jobInput.value = job.textContent;
 function openPopup(evt) {
   evt.preventDefault();
   popup.classList.add('popup_opened');
+  nameInput.value = name.textContent;
+  jobInput.value = job.textContent;
 }
 editButton.addEventListener('click', openPopup);
 
